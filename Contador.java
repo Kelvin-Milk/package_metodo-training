b402ad7dc104c56bd9723c46073fd33c6b000589
Kelvin-Milk <kelvinleite01@gmail.com>
2022-03-06 17:25:11 -0300

    Os dois arquivos

---
+++ Contador.java
@@ -0,0 +1,13 @@
+package Soma;
+
+public class Contador {
+
+	public static String contadorm(int a, int b) {
+		String s = "";
+		for(int i = 0; i <= b; i++) {
+			s += i + " ";
+		}
+		return s;
+	}
+
+}
---
+++ Operacao.java
@@ -0,0 +1,16 @@
+package Soma;
+
+public class Operacao {
+	
+	public static void soma(int a, int b) {
+		int soma = a + b;
+		System.out.print(soma);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		soma(3,5);
+		System.out.print(Contador.contadorm(3,5));
+	}
+
+}
